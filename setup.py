348acc36d107c76d65ea31aab6c246c567322f2c
Kenny2github <kenny2minecraft@gmail.com>
2019-03-24 14:37:49 +0800

    Update to discord.py 1.0.0a1786+g77dd4e3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='kenny2automate',
-	version='3.1',
+	version='3.2',
 	description='A Python Discord bot',
 	url='https://github.com/Kenny2github/kenny2automate',
 	author='Kenny2github',
@@ -16,17 +16,20 @@ setup(
 		'Natural Language :: English',
 		'Natural Language :: Chinese (Traditional)',
 		'Natural Language :: Japanese',
+		'Natural Language :: Russian',
+		'Natural Language :: French',
 		'Operating System :: Microsoft :: Windows :: Windows 10',
-		'Operating System :: POSIX :: Linux'
-		'Programming Language :: Python :: 3.6',
-		'Programming Language :: SQL'
+		'Operating System :: POSIX :: Linux',
+		'Programming Language :: Python :: 3 :: Only',
+		'Programming Language :: Python :: 3.7',
+		'Programming Language :: SQL',
 	],
 	keywords='discord bot fun',
 	packages=find_packages(),
 	install_requires=[
-		'discord.py<=1.0.0a1773+gc30a366',
+		'discord.py<=1.0.0a1786+g77dd4e3',
 		'requests',
 		'aiohttp',
 	],
-	python_requires='>=3.6',
+	python_requires='>=3.7',
 )
